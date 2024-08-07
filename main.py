b13c86ad412777785b3154235099e14bd1a7b374
IthielKyama <nyagaithiel@gmail.com>
2024-08-07 21:46:32 +0300

    code cookie clicker

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://orteil.dashnet.org/experiments/cookie/")
 
 timeout = time.time() + 15
-time_interval = 5
+time_interval = time.time() + 5
 cookie = driver.find_element(By.ID, "cookie")
 
 price = driver.find_elements(By.CSS_SELECTOR, "#store b")
@@ -16,21 +16,18 @@ price.pop(-1)
 price_list = [int(item.text.split("-")[1].replace(",", "")) for item in price]
 print(price_list)
 
-
-
-
 while time.time() < timeout:
     cookie.click()
-    if timeout % time_interval == 0:
-        index = 0
-        balance = int(driver.find_element(By.XPATH, '//*[@id="money"]').text)
-        for item in price_list:
-            if balance < item:
-                balance -= item
-                if balance < 0:
-                    price[index].click()
-            index += 1
-
-
-
+    # if time.time() > time_interval:
+    #     index = 0
+    #     balance = int(driver.find_element(By.XPATH, '//*[@id="money"]').text)
+    #     affordable_upgrades = []
+    #     for item in price_list:
+    #         if balance > item:
+    #             affordable_upgrades.append(item)
+    #             highest_price_item = max(affordable_upgrades)
+    #
+    #             if balance < 0:
+    #                 price[index].click()
+    #         index += 1
 
